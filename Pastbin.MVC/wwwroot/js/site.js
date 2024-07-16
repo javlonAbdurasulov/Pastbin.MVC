ab4e4ae78dee4997a38a7f55470461322d22b817
abdurasulov2203 <abdurasulov2203@gmail.com>
2024-07-16 19:13:04 +0500

    indexsite

--- Pastbin.MVC/wwwroot/js/site.js
+++ Pastbin.MVC/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function toggleForm() {
+    const loginForm = document.getElementById('login-form');
+    const registerForm = document.getElementById('register-form');
+
+    if (loginForm.classList.contains('hidden')) {
+        loginForm.classList.remove('hidden');
+        registerForm.classList.add('hidden');
+    } else {
+        loginForm.classList.add('hidden');
+        registerForm.classList.remove('hidden');
+    }
+}
